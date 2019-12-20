637d6ebc47122a6e4db3b16f474466053d28ccee
Mark Genesis Sargento <gensargento@gmail.com>
2019-12-20 08:33:16 +0800

    Adds Computer class and some methods

--- mastermind.rb
+++ mastermind.rb
@@ -106,34 +106,78 @@ class DecodingBoard
     print ')'
   end
 end
+class Computer < code_breaker
+  attr_accesso :code_maker
+  def initialize(code_maker)
+   @code_maker = code_maker
+  end
+  def give_random_pegs
+    colors = ['yellow','brown','violet','green','blue']
+    
+  end
+end
 class MasterMind
   attr_accessor :code_breaker, :code_maker, :decoding_board
   def initialize
     @code_breaker = CodeBreaker.new
     @code_maker = CodeMaker.new(code_breaker)
+    @computer = Computer.new(code_maker)
     @decoding_board = DecodingBoard.new(code_maker, code_breaker)
     self.code_maker.make_pattern
   end
   def start
-    12.times do
-      puts self.code_maker.pattern
+    while true
+      puts 'Type "cm" if you want to be a code breake else type "cb" if you want to be the code breaker'
+      puts 'Type "exit" if you want to quit'
+      answer = gets.chomp
+      if answer == 'cm'
+        play_as_code_maker
+      elsif answer =='cb'
+        play_as_code_breaker
+        break
+      elsif answer.downcase == 'exit'
+        break
+      end
+    end
+
+
+  end
+  private
+  def play_as_code_maker
+    command = ''
+    puts ' Make a pattern that consists of 4 pegsand separate it with space'
+    puts 'Choices: blue green yellow brown violet'
+    pattern = gets.chomp.split(' ')
+    @code_maker.pattern = pattern
+
+    12.times do |attempt|
       @decoding_board.draw
-      code_breaker.guess_code
+      command = code_breaker.guess_code
+    end
+  end
+  def play_as_code_breaker
+    command= ""
+    12.times do |attempt| 
+      @decoding_board.draw
+      command = code_breaker.guess_code
       code_maker.give_feedback
       if is_correct?
         puts is_correct?
         break
       end
-      if self.code_breaker.code_pegs.length != 4
+      if command.include?('exit')
+        break
+      elsif
+        self.code_breaker.code_pegs.length != 4
         self.code_maker.feedback.clear
         self.code_breaker.code_pegs.clear
         puts 'You can only input 4 pegs'
         redo
       end
+      puts "Attempt: #{attempt + 1}/12"
       decoding_board.draw
       @code_maker.feedback.clear
       @code_breaker.code_pegs.clear
-
     end
   end
   private
@@ -147,9 +191,7 @@ class MasterMind
     end
     return(correct_count == 4)
   end
-  def show_answer
 
-  end
 end
 mastermind = MasterMind.new
 mastermind.start
