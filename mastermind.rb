3756751ba441f92827badb1474838f8a57d79997
Genesis <genesistobiassargento@gmail.com>
2019-12-26 09:11:25 +0800

    Fix for the give_feedback method

--- mastermind.rb
+++ mastermind.rb
@@ -3,43 +3,73 @@ class Peg
   def initialize(color)
     @color = color
   end
-  def to_string
+  def to_s
     return("#{color}")
   end
-  end
+
+end
+
 class Player
-  attr_accessor :pattern, :feedback
+  attr_accessor :pattern, :feedback, :code_pegs
+
   def initialize
     @panttern = []
     @feedback = []
+    @code_pegs = []
   end
+
   def guess_code()
     puts 'Input your code and separate it with spaces'
     guess = gets.chomp.split(' ')
-    pegs = []
     guess.each do |color|
-      pegs.push(Peg.new(color))
+      code_pegs.push(Peg.new(color))
     end
-    return(pegs)
   end
 end
 
 class Computer
   attr_accessor :pattern, :feedback
+
   def initialize
       @pattern = []
       @feedback = []
-    end
+  end
+
   def make_random_pattern
     5.times do
-      pattern.push(get_random_pegs)
+      pattern.push(random_pegs)
+    end
   end
+  def give_feedback(code_pegs)
+    code_pegs.each_with_index do |peg, index|
+      if present?(peg)
+        if correct_in_position?(index, peg)
+          feedback.push('B')
+        else
+          feedback.push('W')
+        end
+      end
+    end
+  end
+
   private
-  def get_random_pegs
+
+  def present?(code_peg)
+    pattern.each do |peg|
+      return true if code_peg.color == peg.color
+    end
+  end
+
+  def correct_in_position?(index, code_peg)
+    return true if code_peg.color == pattern[index].color
+  end
+
+  def random_pegs
     colors = ['Red, Blue, Yellow, Green, Violet']
     index = Random.rand(4)
-    return(Pegs.new(colors[index])
+    Peg.new(colors[index])
   end
+
 end
 
 class DecodingBoard
@@ -77,7 +107,29 @@ class Mastermind
   def initialize
     @player = Player.new
     @computer = Computer.new
-    @decoding_board;
+    @decoding_board = ''
+  end
+  def play_as_code_breaker
+    decoding_board = DecodingBoard.new(computer,player)
+    puts 'The game has started'
+    puts 'You will play as the code breaker and the computer as code maker'
+    puts 'Input 4 colors and separate it with spaces'
+    computer.make_random_pattern
+    puts computer.pattern
+    12.times do
+      puts 'Choices: blue, red, yellow, green, violet'
+      player.guess_code
+      if player.code_pegs.length != 4
+        puts 'Please input 4 pegs and separate it with spaces'
+        redo
+      end
+      computer.give_feedback(player.code_pegs)
+      decoding_board.draw
+      player.code_pegs.clear
+      computer.feedback.clear
+    end
   end
 
 end
+a = Mastermind.new
+a.play_as_code_breaker
